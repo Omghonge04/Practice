c05c08072e6bfedb9b8fcf16c598b36317528ee1
Omghonge04 <omghonge4@gmail.com>
2025-12-29 23:14:50 +0530

    Add files via upload

---
+++ Tableoffive.java
@@ -0,0 +1,31 @@
+//using for loop
+public class Tableoffive {
+  public static void main(String[] args) {
+	for(int i = 1; i <= 10; i++) {
+		System.out.println("5 x " + i + " = " + (5*i));
+}
+	
+	//using while loop
+	int i = 1;
+	while(i<=10) {
+		System.out.println("5 x " + i + " = " + (5*i));
+	}
+	
+	//using do while loop
+    int j = 1;
+    do {
+        System.out.println( "5 x " + j + " = " + (5 * j));
+        j++;
+    } while (j <= 10);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
