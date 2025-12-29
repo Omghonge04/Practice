6f07b6f67aa86e4533f3b5a64c8ff1229b06f1e9
Omghonge04 <omghonge4@gmail.com>
2025-12-29 23:00:25 +0530

    Add files via upload

---
+++ Printnum.java
@@ -0,0 +1,32 @@
+
+public class Printnum {
+	public static void main(String[] args) {
+		
+
+		        // Using for loop
+		        System.out.println("Using for loop:");
+		        for (int i = 1; i <= 100; i++) {
+		            System.out.println(i);
+		        }
+
+		        // Using while loop
+		        System.out.println("\nUsing while loop:");
+		        int j = 1;
+		        while (j <= 100) {
+		            System.out.println(j);
+		            j++;
+		        }
+
+		        // Using do-while loop
+		        System.out.println("\nUsing do-while loop:");
+		        int k = 1;
+		        do {
+		            System.out.println(k);
+		            k++;
+		        } while (k <= 100);
+		    }
+		
+
+	}
+
+
